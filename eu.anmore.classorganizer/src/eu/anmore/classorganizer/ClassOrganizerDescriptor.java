ec21aa1cd4f73e7163954a2316925846c79515c6
mmiedzinski <marcin.miedzinski@gmail.com>
2013-01-31 20:55:00 +0100

    Full order

--- eu.anmore.classorganizer/src/eu/anmore/classorganizer/ClassOrganizerDescriptor.java
+++ eu.anmore.classorganizer/src/eu/anmore/classorganizer/ClassOrganizerDescriptor.java
@@ -7,21 +7,77 @@ package eu.anmore.classorganizer;
  */
 public final class ClassOrganizerDescriptor {
 
-	static final int PUBLIC_METHOD = 0;
+	static final int PUBLIC_STATIC_METHOD = 0;
 
-	static final int PUBLIC_FIELD = 1;
+	static final int PUBLIC_STATIC_FIELD = 1;
 
-	static final int DEFAULT_METHOD = 2;
+	static final int PUBLIC_STATIC_INITIALIZER = 2;
 
-	static final int DEFAULT_FIELD = 3;
+	static final int PUBLIC_STATIC_TYPE = 3;
 
-	static final int PROTECTED_METHOD = 4;
+	static final int PUBLIC_CONSTRUCTOR = 4;
 
-	static final int PROTECTED_FIELD = 5;
+	static final int PUBLIC_METHOD = 5;
 
-	static final int PRIVATE_METHOD = 6;
+	static final int PUBLIC_FIELD = 6;
 
-	static final int PRIVATE_FIELD = 7;
+	static final int PUBLIC_INITIALIZER = 7;
+
+	static final int PUBLIC_TYPE = 8;
+
+	static final int PROTECTED_STATIC_METHOD = 9;
+
+	static final int PROTECTED_STATIC_FIELD = 10;
+
+	static final int PROTECTED_STATIC_INITIALIZER = 11;
+
+	static final int PROTECTED_STATIC_TYPE = 12;
+
+	static final int PROTECTED_CONSTRUCTOR = 13;
+
+	static final int PROTECTED_METHOD = 14;
+
+	static final int PROTECTED_FIELD = 15;
+
+	static final int PROTECTED_INITIALIZER = 16;
+
+	static final int PROTECTED_TYPE = 17;
+
+	static final int DEFAULT_STATIC_METHOD = 18;
+
+	static final int DEFAULT_STATIC_FIELD = 19;
+
+	static final int DEFAULT_STATIC_INITIALIZER = 20;
+
+	static final int DEFAULT_STATIC_TYPE = 21;
+
+	static final int DEFAULT_CONSTRUCTOR = 22;
+
+	static final int DEFAULT_METHOD = 23;
+
+	static final int DEFAULT_FIELD = 24;
+
+	static final int DEFAULT_INITIALIZER = 25;
+
+	static final int DEFAULT_TYPE = 26;
+
+	static final int PRIVATE_STATIC_METHOD = 27;
+
+	static final int PRIVATE_STATIC_FIELD = 28;
+
+	static final int PRIVATE_STATIC_INITIALIZER = 29;
+
+	static final int PRIVATE_STATIC_TYPE = 30;
+
+	static final int PRIVATE_CONSTRUCTOR = 31;
+
+	static final int PRIVATE_METHOD = 32;
+
+	static final int PRIVATE_FIELD = 33;
+
+	static final int PRIVATE_INITIALIZER = 34;
+
+	static final int PRIVATE_TYPE = 35;
 
 	static final String CLEANUP_ID = "cleanup.update";
 
--- eu.anmore.classorganizer/src/eu/anmore/classorganizer/ModifierResolverImpl.java
+++ eu.anmore.classorganizer/src/eu/anmore/classorganizer/ModifierResolverImpl.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Modifier;
 
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.BodyDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 /**
  * Implementation of {@link ModifierResolver}.
@@ -18,40 +19,212 @@ public class ModifierResolverImpl implements ModifierResolver {
 			return resolveMethod(bodyDeclaration);
 		} else if (isFieldDeclaration(bodyDeclaration)) {
 			return resolveField(bodyDeclaration);
+		} else if (isInitializerDeclaration(bodyDeclaration)) {
+			return resolveInitializer(bodyDeclaration);
+		} else if (isTypeDeclaration(bodyDeclaration)) {
+			return resolveType(bodyDeclaration);
+		} else if (isConstructorDeclaration(bodyDeclaration)) {
+			return resolveConstructor(bodyDeclaration);
 		}
 		return 0;
 	}
 
 	private int resolveMethod(BodyDeclaration bodyDeclaration) {
 		if (isPublic(bodyDeclaration)) {
-			return ClassOrganizerDescriptor.PUBLIC_METHOD;
+			return resolvePublicMethod(bodyDeclaration);
 		} else if (isProtected(bodyDeclaration)) {
-			return ClassOrganizerDescriptor.PROTECTED_METHOD;
+			return resolveProtectedMethod(bodyDeclaration);
 		} else if (isPrivate(bodyDeclaration)) {
-			return ClassOrganizerDescriptor.PRIVATE_METHOD;
+			return resolvePrivateMethod(bodyDeclaration);
 		} else {
-			return ClassOrganizerDescriptor.DEFAULT_METHOD;
+			return resolveDefaultMethod(bodyDeclaration);
 		}
+	}
+
+	private int resolveDefaultMethod(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.DEFAULT_STATIC_METHOD;
+		}
+		return ClassOrganizerDescriptor.DEFAULT_METHOD;
+	}
+
+	private int resolvePrivateMethod(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PRIVATE_STATIC_METHOD;
+		}
+		return ClassOrganizerDescriptor.PRIVATE_METHOD;
+	}
+
+	private int resolveProtectedMethod(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PROTECTED_STATIC_METHOD;
+		}
+		return ClassOrganizerDescriptor.PROTECTED_METHOD;
+	}
+
+	private int resolvePublicMethod(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PUBLIC_STATIC_METHOD;
+		}
+		return ClassOrganizerDescriptor.PUBLIC_METHOD;
 	}
 
 	private int resolveField(BodyDeclaration bodyDeclaration) {
 		if (isPublic(bodyDeclaration)) {
-			return ClassOrganizerDescriptor.PUBLIC_FIELD;
+			return resolvePublicField(bodyDeclaration);
 		} else if (isProtected(bodyDeclaration)) {
-			return ClassOrganizerDescriptor.PROTECTED_FIELD;
+			return resolveProtectedField(bodyDeclaration);
 		} else if (isPrivate(bodyDeclaration)) {
-			return ClassOrganizerDescriptor.PRIVATE_FIELD;
+			return resolvePrivateField(bodyDeclaration);
 		} else {
-			return ClassOrganizerDescriptor.DEFAULT_FIELD;
+			return resolveDefaultField(bodyDeclaration);
 		}
 	}
 
+	private int resolveDefaultField(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.DEFAULT_STATIC_FIELD;
+		}
+		return ClassOrganizerDescriptor.DEFAULT_FIELD;
+	}
+
+	private int resolvePrivateField(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PRIVATE_STATIC_FIELD;
+		}
+		return ClassOrganizerDescriptor.PRIVATE_FIELD;
+	}
+
+	private int resolveProtectedField(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PROTECTED_STATIC_FIELD;
+		}
+		return ClassOrganizerDescriptor.PROTECTED_FIELD;
+	}
+
+	private int resolvePublicField(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PUBLIC_STATIC_FIELD;
+		}
+		return ClassOrganizerDescriptor.PUBLIC_FIELD;
+	}
+
+	private int resolveInitializer(BodyDeclaration bodyDeclaration) {
+		if (isPublic(bodyDeclaration)) {
+			return resolvePublicInitializer(bodyDeclaration);
+		} else if (isProtected(bodyDeclaration)) {
+			return resolveProtectedInitializer(bodyDeclaration);
+		} else if (isPrivate(bodyDeclaration)) {
+			return resolvePrivateInitializer(bodyDeclaration);
+		} else {
+			return resolveDefaultInitializer(bodyDeclaration);
+		}
+	}
+
+	private int resolveDefaultInitializer(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.DEFAULT_STATIC_INITIALIZER;
+		}
+		return ClassOrganizerDescriptor.DEFAULT_INITIALIZER;
+	}
+
+	private int resolvePrivateInitializer(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PRIVATE_STATIC_INITIALIZER;
+		}
+		return ClassOrganizerDescriptor.PRIVATE_INITIALIZER;
+	}
+
+	private int resolveProtectedInitializer(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PROTECTED_STATIC_INITIALIZER;
+		}
+		return ClassOrganizerDescriptor.PROTECTED_INITIALIZER;
+	}
+
+	private int resolvePublicInitializer(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PUBLIC_STATIC_INITIALIZER;
+		}
+		return ClassOrganizerDescriptor.PUBLIC_INITIALIZER;
+	}
+
+	private int resolveConstructor(BodyDeclaration bodyDeclaration) {
+		if (isPublic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PUBLIC_CONSTRUCTOR;
+		} else if (isProtected(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PROTECTED_CONSTRUCTOR;
+		} else if (isPrivate(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PRIVATE_CONSTRUCTOR;
+		} else {
+			return ClassOrganizerDescriptor.DEFAULT_CONSTRUCTOR;
+		}
+	}
+
+	private int resolveType(BodyDeclaration bodyDeclaration) {
+		if (isPublic(bodyDeclaration)) {
+			return resolvePublicType(bodyDeclaration);
+		} else if (isProtected(bodyDeclaration)) {
+			return resolveProtectedType(bodyDeclaration);
+		} else if (isPrivate(bodyDeclaration)) {
+			return resolvePrivateType(bodyDeclaration);
+		} else {
+			return resolveDefaultType(bodyDeclaration);
+		}
+	}
+
+	private int resolveDefaultType(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.DEFAULT_STATIC_TYPE;
+		}
+		return ClassOrganizerDescriptor.DEFAULT_TYPE;
+	}
+
+	private int resolvePrivateType(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PRIVATE_STATIC_TYPE;
+		}
+		return ClassOrganizerDescriptor.PRIVATE_TYPE;
+	}
+
+	private int resolveProtectedType(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PROTECTED_STATIC_TYPE;
+		}
+		return ClassOrganizerDescriptor.PROTECTED_TYPE;
+	}
+
+	private int resolvePublicType(BodyDeclaration bodyDeclaration) {
+		if (isStatic(bodyDeclaration)) {
+			return ClassOrganizerDescriptor.PUBLIC_STATIC_TYPE;
+		}
+		return ClassOrganizerDescriptor.PUBLIC_TYPE;
+	}
+
 	private boolean isMethodDeclaration(BodyDeclaration bodyDeclaration) {
-		return bodyDeclaration.getNodeType() == ASTNode.METHOD_DECLARATION;
+		return bodyDeclaration.getNodeType() == ASTNode.METHOD_DECLARATION
+				&& !((MethodDeclaration) bodyDeclaration).isConstructor();
+	}
+
+	private boolean isConstructorDeclaration(BodyDeclaration bodyDeclaration) {
+		return bodyDeclaration.getNodeType() == ASTNode.METHOD_DECLARATION
+				&& ((MethodDeclaration) bodyDeclaration).isConstructor();
+	}
+
+	private boolean isStatic(BodyDeclaration bodyDeclaration) {
+		return Modifier.isStatic(bodyDeclaration.getModifiers());
 	}
 
 	private boolean isFieldDeclaration(BodyDeclaration bodyDeclaration) {
 		return bodyDeclaration.getNodeType() == ASTNode.FIELD_DECLARATION;
+	}
+
+	private boolean isInitializerDeclaration(BodyDeclaration bodyDeclaration) {
+		return bodyDeclaration.getNodeType() == ASTNode.INITIALIZER;
+	}
+
+	private boolean isTypeDeclaration(BodyDeclaration bodyDeclaration) {
+		return bodyDeclaration.getNodeType() == ASTNode.TYPE_DECLARATION;
 	}
 
 	private boolean isPrivate(BodyDeclaration bodyDeclaration) {
